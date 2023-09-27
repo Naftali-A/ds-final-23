f69a6a240466f38fb42e4f57fafdab11d27f27c3
EytanSlotnik <eythens@gmail.com>
2023-09-27 13:46:05 +0300

    Adding data structure

--- ds-final-23/unpack_data.py
+++ ds-final-23/unpack_data.py
@@ -3,16 +3,23 @@ import shutil
 import os
 import fnmatch
 
-def gunzip(file_path,output_path):
-    with gzip.open(file_path,"rb") as f_in, open(output_path,"wb") as f_out:
+
+def gunzip(file_path, output_path):
+    with gzip.open(file_path, "rb") as f_in, open(output_path, "wb") as f_out:
         shutil.copyfileobj(f_in, f_out)
+
 
 def recurse_and_gunzip(root):
     walker = os.walk(root)
-    for root,dirs,files in walker:
+    print(list(walker))
+    for root, dirs, files in walker:
+        print(root, dirs, files)
         for f in files:
-            if fnmatch.fnmatch(f,"*.gz"):
+            print(f, root)
+            if fnmatch.fnmatch(f, "*.gz"):
                 print("gunzipping {}".format(f))
-                gunzip(root+"/"+f, root+"/"+f.replace(".gz",""))
+                gunzip(root + "/" + f, root + "/" + f.replace(".gz", ""))
+
 
 recurse_and_gunzip('data')
+
